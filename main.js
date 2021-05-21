4662ea65756266e73cf753445473a494e829d857
john19294 <76793074+john19294@users.noreply.github.com>
2021-05-21 14:46:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var SpeechRecognition=window.webkitSpeechRecognition;//webkitspeechrecognition is the api 
+var recognition=new SpeechRecognition();
+function start(){
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+}
+recognition.onresult = function run (event) {
+console.log(event);
+var Content=event.results[0][0].transcript;
+console.log(Content);
+document.getElementById("textbox").innerHTML=Content};
